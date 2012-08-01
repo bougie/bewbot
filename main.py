3df028c7045c8cadd1b596c94ce2c8aa4e712eb1
David H <bougieskater@gmail.com>
2012-08-01 16:50:55 +0200

    Initialisation de la gestion des modules

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import subprocess
 import lib.irclib as irclib
 import lib.ircbot as ircbot
+from module import *
 
 pub_command_list = ['quote']
 priv_command_list = ['quit']
@@ -14,35 +15,30 @@ def command_exists(list, name):
             return True
             
     return False
-    
+
 def can_exec(cmd, pseudo):
     return True
-    
-class Quote:
-    def __init__(self):
-        pass
-        
-    def add(self):
-        pass
-
-    def get(self):
-        pass
 
 class Bewbot(ircbot.SingleServerIRCBot):
     def __init__(self, servers, chans, pseudo, admins):
         self.adminsuser = admins
         self.chans = chans
         self.pseudo = pseudo
+        self.modules = dict()
 
         ircbot.SingleServerIRCBot.__init__(self,
             servers,
             pseudo,
             "Bot ultrasupra awesome")
+            
+    def load_modules(self, list):
+        pass
 
     def on_welcome(self, srv, evt):
         """Connected to the server"""
         
-        srv.join(self.chans)
+        for chan in self.chans:
+            srv.join(chan)
 
     def on_join(self, srv, evt):
         """Method called when a user join a chan"""
@@ -72,11 +68,16 @@ class Bewbot(ircbot.SingleServerIRCBot):
 # Program main function
 #
 def main():
+    servers = [("roubaix2.fr.epiknet.org", 6667)]
     pseudo = "gridania"
-    chan = "#hugland"
+    chan = ["#hugland"]
     admins = ['bougie']
+    modules = ['quote']
 
-    Bewbot([("roubaix2.fr.epiknet.org", 6667)], chan, pseudo, admins).start()
+    Bewbot bot = Bewbot(servers, chan, pseudo, admins)
+    
+    bot.load_modules(modules)
+    bot.start()
 
 if __name__ == "__main__":
     main()
---
+++ module/quote.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+class Quote:
+    def __init__(self):
+        pass
+        
+    def add(self):
+        pass
+
+    def get(self):
+        pass
