25cba8db9894a08f3a9de16b559a792e7fd5f216
bougie <bougie@appartland.eu>
2014-05-05 21:36:28 +0200

    Decodage des entite HTML dans le titre de la page

--- module/url.py
+++ module/url.py
@@ -2,33 +2,37 @@
 
 import re
 import urllib2
+import HTMLParser
 
 def parser(srv, chan, line):
-    obj = re.match('.*(http[s]?://(.+[a-zA-Z0-9/\._-]))\s.*', line, flags=re.DOTALL | re.UNICODE)
-    
-    if obj == None:
-        obj = re.match('.*(http[s]?://(.+[a-zA-Z0-9/\._-]))$', line, flags=re.DOTALL | re.UNICODE)
-    
-    if obj == None:
-        obj = re.match('(http[s]?://(.+[a-zA-Z0-9/\._-]))$', line, flags=re.DOTALL | re.UNICODE)
-    
-    if obj != None:
-        try:
-            content = urllib2.urlopen(url=obj.group(1), timeout=5).read()
-            
-            if len(content) > 0:
-                obj2 = re.search('<title>(.*)</title>', content, flags=re.IGNORECASE | re.DOTALL | re.UNICODE)
+	obj = re.match('.*(http[s]?://(.+[a-zA-Z0-9/\._-]))\s.*', line, flags=re.DOTALL | re.UNICODE)
 
-                if obj2 != None:
-                    t = obj2.group(1).split("\n")
-                    title = ""
-                    
-                    for x in t:
-                        if len(title) == 0:
-                            title = x.strip()
-                        else:
-                            title = title + " " + x.strip()
+	if obj == None:
+		obj = re.match('.*(http[s]?://(.+[a-zA-Z0-9/\._-]))$', line, flags=re.DOTALL | re.UNICODE)
 
-                    srv.privmsg(chan, '[Link Info] title : ' + title)
-        except:
-            pass
+	if obj == None:
+		obj = re.match('(http[s]?://(.+[a-zA-Z0-9/\._-]))$', line, flags=re.DOTALL | re.UNICODE)
+
+	if obj != None:
+		try:
+			content = urllib2.urlopen(url=obj.group(1), timeout=5).read()
+
+			if len(content) > 0:
+				obj2 = re.search('<title>(.*)</title>', content, flags=re.IGNORECASE | re.DOTALL | re.UNICODE)
+
+				if obj2 != None:
+					t = obj2.group(1).split("\n")
+					title = ""
+
+					for x in t:
+						if len(title) == 0:
+							title = x.strip()
+						else:
+							title = title + " " + x.strip()
+
+					h = HTMLParser.HTMLParser()
+					title = h.unescape(title)
+
+					srv.privmsg(chan, '[Link Info] title : ' + title)
+		except:
+			pass
